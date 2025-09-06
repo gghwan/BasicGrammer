51dda776ff7e7da33495d38fa21b3a7813695302
Gim Gyeong Hwan <gghwan93@gmail.com>
2025-09-06 23:49:48 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250906/윤년인가/is-leap-year.cpp
+++ 250906/윤년인가/is-leap-year.cpp
@@ -2,22 +2,14 @@
 using namespace std;
 
 int main() {
-    // Please write your code here.
     int Y;
-
     cin >> Y;
-
-    if( Y % 4 == 0 ){
-        if(Y % 100 != 0){
+    
+    if ((Y % 4 == 0 && Y % 100 != 0) || (Y % 400 == 0)) {
         cout << "true";
-        }
-    else{
-        if( Y % 100 == 0){
-            if(Y % 400 != 0){
-                cout << "false";
-        }            
+    } else {
+        cout << "false";
     }
-    }
-    }
+    
     return 0;
 }
