81b8892632066f5528b4802ce19801dea6eb62e2
Gim Gyeong Hwan <gghwan93@gmail.com>
2025-09-06 23:27:18 +0900

    Title: 남녀노소 구분짓기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250906/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int age;
+    int gender;
+
+    cin >> gender;
+    cin >> age;
+
+    if ( gender == 0 ){
+        if( age >= 19){
+            cout << "MAN";
+        }
+        else{
+            cout << "BOY";
+        }
+    }
+    else{
+        if( age >= 19){
+            cout << "WOMAN";
+        }
+        else{
+            cout << "GIRL";
+        }
+    }
+        
+    return 0;
+}
