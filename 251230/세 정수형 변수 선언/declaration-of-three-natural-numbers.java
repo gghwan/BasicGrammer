eafe70fc2058f45bbea27ad9ac1b9be611f4eaa8
Gim Gyeong Hwan <gghwan93@gmail.com>
2025-12-30 22:53:41 +0900

    Title: 세 정수형 변수 선언, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251230/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7;
+        int b = 23;
+        int c = a + b;
+        System.out.printf("7 + 23 = " + c);
+    }
+}
