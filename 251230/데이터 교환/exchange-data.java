b5ab9bbae18791a06da63200bb3cd0c0b57b31de
Gim Gyeong Hwan <gghwan93@gmail.com>
2025-12-30 23:59:31 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251230/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 6, c = 7;
+        int temp1;
+        int temp2;
+
+        temp1 = a;
+        temp2 = b;
+        a = c;
+        b = temp1;
+        c = temp2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
