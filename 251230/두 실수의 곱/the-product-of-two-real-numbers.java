a47dc5686158b73ebd64cd8b3a31461a2136e877
Gim Gyeong Hwan <gghwan93@gmail.com>
2025-12-30 23:15:34 +0900

    Title: 두 실수의 곱, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251230/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double a = 5.26;
+        double b = 8.27;
+        double c = a * b;
+        System.out.printf("%.3f", c);
+    }
+}
