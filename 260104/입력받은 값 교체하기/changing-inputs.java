20ccbd86c150a92178223a13e3fc4b9f24497299
Gim Gyeong Hwan <gghwan93@gmail.com>
2026-01-04 23:17:20 +0900

    Title: 입력받은 값 교체하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260104/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int temp = sc.nextInt();
+
+        int temp = b;
+        int a = b;
+        int b = temp;
+
+        System.out.print(a);
+        System.out.print(b);
+    }
+}
