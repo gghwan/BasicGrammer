d63a81a21b90d6a9b3880ffb99e28c924533805c
Gim Gyeong Hwan <gghwan93@gmail.com>
2026-01-04 23:23:59 +0900

    Title: 입력받은 값 교체하기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 260104/입력받은 값 교체하기/changing-inputs.java
+++ 260104/입력받은 값 교체하기/changing-inputs.java
@@ -5,13 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int temp = sc.nextInt();
 
-        int temp = b;
-        int a = b;
-        int b = temp;
-
-        System.out.print(a);
-        System.out.print(b);
+        System.out.println(b + " " + a);
     }
 }
